01fb6f6eceb4424a766ee15f2a8e0e1929d10ac0
Kevin.Cai <qingshen.cai@yz-intelligence.com>
2021-10-16 11:42:13 +0800

    refactor:
    - 链式时间基础库
    - 支持Morning、EndNight、AnyDayMorning、AnyDayEndNight（常用类型互转）
    - 结果支持： String()、Timestamp()、Time()

--- date/date.go
+++ date/date.go
@@ -1,14 +1,20 @@
 package date
 
+import "time"
+
 type Format string
 
 const (
+	//YMDHmsCN 中文年月日时分秒
+	YMDHmsCN Format = "2006年01月02日 15时04分05秒"
+	//YmdCN 中文年月日
+	YmdCN Format = "2006年01月02日"
 	//YMDHms 年-月-日 时:分:秒
-	YMDHms Format = "2006-01-02 15:04:06"
+	YMDHms Format = "2006-01-02 15:04:05"
 	//Ymd 年-月-日
 	Ymd Format = "2006-01-02"
 	//YMDHmsSlash 年/月/日 时:分:秒
-	YMDHmsSlash Format = "2006/01/02 15:04:06"
+	YMDHmsSlash Format = "2006/01/02 15:04:05"
 	//YmdSlash 年/月/日
 	YmdSlash Format = "2006/01/02"
 )
@@ -21,11 +27,30 @@ func (d Format) Value() string {
 type Location string
 
 const (
-	CST   Location = "CST"
-	UTC   Location = "UTC"
-	Local Location = "Local"
+	CST      Location = "CST"
+	ShangHai Location = "Asia/Shanghai"
+	UTC      Location = "UTC"
+	Local    Location = "Local"
 )
 
 func (l Location) Value() string {
 	return string(l)
+}
+
+var Zero = time.Time{}
+
+type OneTime string
+
+const (
+	OneDayMorning OneTime = " 00:00:00"
+
+	OneDayEndNight OneTime = " 23:59:59"
+)
+
+func (o OneTime) Value() string {
+	return string(o)
+}
+
+func (o OneTime) Join(val string) string {
+	return val + o.Value()
 }
