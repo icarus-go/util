10ade4ea49b4aafd1b199660cdedfdd367951c43
KevinCC <kevinicarus@qq.com>
2021-11-11 22:10:17 +0800

    refactor:
    - 仅仅分隔符有差异

--- date/time_test.go
+++ date/time_test.go
@@ -18,21 +18,21 @@ func Test_date_Morning(t *testing.T) {
 
 }
 
-func Test_date_MorningByTime(t *testing.T) {
-
-	time := time.Now()
-
-	s := Now().AnyTimeMorning(time).String()
-	timestamp := Now().AnyTimeMorning(time).Timestamp()
-	timeValue := Now().AnyTimeMorning(time).TimeValue()
-
-	t.Logf("字符串: %s", s)
-
-	t.Logf("time.Time : %d", timeValue.Unix())
-
-	t.Logf("时间戳: %d", timestamp)
-
-}
+//func Test_date_MorningByTime(t *testing.T) {
+//
+//	time := time.Now()
+//
+//	s := Now().AnyTimeMorning(time).String()
+//	timestamp := Now().AnyTimeMorning(time).Timestamp()
+//	timeValue := Now().AnyTimeMorning(time).TimeValue()
+//
+//	t.Logf("字符串: %s", s)
+//
+//	t.Logf("time.Time : %d", timeValue.Unix())
+//
+//	t.Logf("时间戳: %d", timestamp)
+//
+//}
 
 func Test_date_Add(t *testing.T) {
 
